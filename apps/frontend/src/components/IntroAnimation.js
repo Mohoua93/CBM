a608a401c880b0e2fc85676b0f0fbd34faeb4e04
Mohoua93 <oualid.mohamed.oualid@gmail.com>
2026-04-06 20:16:43 +0200

    change intro animation

--- apps/frontend/src/components/IntroAnimation.js
+++ apps/frontend/src/components/IntroAnimation.js
@@ -1,18 +1,14 @@
 import "../styles/IntroAnimation.css";
-import logo from "../assets/logo-anim.png";
+import logo from "../assets/Logo-transp.png";
 
 function IntroAnimation() {
   return (
     <div className="intro" aria-hidden="true">
-      <div className="intro__content">
-        <img src={logo} alt="CBM" className="intro__logo" />
-
-        <div className="intro__line-wrap">
-          <span className="intro__line"></span>
-        </div>
-
-        <p className="intro__text">service and car - chargement</p>
-      </div>
+      <img
+        className="intro__logo"
+        src={logo}
+        alt="C.B.M Services & Cars"
+      />
     </div>
   );
 }
